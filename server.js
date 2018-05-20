c6870d57db3ea502cf68754e496cb3201355d76c
sunchengzhou <scz20160220@163.com>
2018-05-20 19:52:20 +0800

    about jade

--- server.js
+++ server.js
@@ -1,21 +1,5 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cookieSession =require('cookie-session')
-const app = express();
+const jade = require('jade');
 
-let arr = [];
-for(let i=0;i<1000;i++) {
-  arr.push('ios'+ Math.random()*100);
-}
-app.use(cookieParser());
-app.use(cookieSession({
-  name:'sess',
-  keys: arr,
-  maxAge:24*3600*1000
-}))
+const str = jade.renderFile('./www/1.jade')
 
-app.use('/',function(req,res) {
-  req.session.count = 99;
-  res.send('ok')
-})
-app.listen(9000)
+console.log(str)
